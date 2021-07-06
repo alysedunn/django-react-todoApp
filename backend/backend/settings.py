f39fb4f451a726bdefd521ad0b23e30e91916c49
Alyse Dunn <alyse.dunn@gmail.com>
2021-07-06 13:38:53 -0400

    Remove secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4oyeid-%sz48+!02(l6)v_)y(uxo4^5^0o3+^_71r1btg-+^a#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
